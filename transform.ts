5a4d7adb257a7aea53b127032abd2f71c1161dd0
Tricked <tricked@tricked.pro>
2024-03-15 18:49:51 +0100

    fix: remove before century and skip prints

--- transform.ts
+++ transform.ts
@@ -1,12 +1,23 @@
-import { encode, decode } from "cborg";
+import { decode, encode } from "cborg";
 const data = await Bun.file("./data/5k.json").json();
 
-const optimized = data.data.map((item) => [
-  item.date_created,
-  item.primary_name,
-  item.manifest.thumbUuid,
-  item.culture?.[0] || "Unknown",
-]);
+const filters: ((date: string) => boolean)[] = [
+  function filterCenturies(date) {
+    return !date.includes("century");
+  },
+  function filterBefore(date) {
+    return !date.includes("before");
+  },
+];
+
+const optimized = data.data
+  .filter((item) => filters.every((filter) => filter(item.date_created)))
+  .map((item) => [
+    item.date_created.split(";")[0],
+    item.primary_name,
+    item.manifest.thumbUuid,
+    item.culture?.[0] || "Unknown",
+  ]);
 
 // images can be reconstructed with: https://media.getty.edu/iiif/image/<id>/full/!500,500/0/default.jpg
 
